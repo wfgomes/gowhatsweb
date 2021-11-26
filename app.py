300d52a37896d5856ca3ca73c54ea65c10552d0a
Wesckley Faria Gomes <wesckley.gomes@tce.se.gov.br>
2021-11-26 10:09:33 -0300

    adding layout

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app =Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        form = request.form['telefone']
-        link = 'https://api.whatsapp.com/send?phone=55' + form
+        tel = request.form['telefone']
+        link = 'https://api.whatsapp.com/send?phone=55' + tel
         return redirect(link)
     else: 
         return render_template('index.html')
